2f09f9cc43d6c32d6481712ac3f12d16b076ad5b
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-22 00:30:57 +0000

    Re-making get_size() logics. Now supporting folders.

--- utlib/files_utils.py
+++ utlib/files_utils.py
@@ -1,17 +1,23 @@
 import os
+from pathlib import Path
 
 
-def get_size(path: str):
-    """Returns the size of a file in bytes.
+def get_size(path):
+    """Returns the size of a file or folder in bytes.
     Args:
         path (str): The path to the file.
     Returns:
         int: The size of the file in bytes.
         FileNotFoundError: If the specified file does not exist.
     """
+    if type(path) == str:
+        total_size = 0
+        path = Path(path)
 
-    try:
-        size = os.path.getsize(path)
-        return size
-    except FileNotFoundError:
-        raise FileNotFoundError
+        for file in path.rglob('*'):
+            if file.is_file():
+                total_size += file.stat().st_size
+
+        return total_size
+    else:
+        return ValueError
