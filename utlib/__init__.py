b7d5b65ac86b73dfabe84b03fccbb8b020b85282
MyroslavRepin <myroslavrepin@gmail.com>
2025-06-03 14:45:18 -0600

    Bump version to 0.3.4 in __init__.py

--- utlib/__init__.py
+++ utlib/__init__.py
@@ -6,7 +6,7 @@ from .strings_utils import remove_vowels
 from .strings_utils import count_words
 from .files_utils import get_size
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 __all__ = ['digit_sum', 'average',
            'is_palindrome', 'vowels', 'remove_vowels', 'count_words', 'get_size']
